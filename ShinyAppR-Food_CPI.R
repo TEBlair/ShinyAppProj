69630a136d95f27682d99455c8344cb160f37ff5
erica-prog <erikadata7@gmail.com>
2025-03-27 18:42:18 -0400

    Update ShinyAppR-Food_CPI.R

--- ShinyAppR-Food_CPI.R
+++ ShinyAppR-Food_CPI.R
@@ -13,7 +13,9 @@ df$Date <- as.Date(df$Date)
 ui <- fluidPage(
   titlePanel("Shiny App 2025: US Consumer Price Index (Food at Home) Inflation"),
   setBackgroundColor(
-    color = c("#F6F8FB")
+    color = c("#FFFFFF", "#90EE91"),
+    gradient = "linear",
+    direction = "left"
   ),
   h3("Erika, Melissa, Thomas"),
   sidebarLayout(
@@ -24,7 +26,6 @@ ui <- fluidPage(
                   max = max(df$Date), 
                   value = range(df$Date),
                   timeFormat = "%Y-%m"),
-      
       selectInput("food_category", 
                   "Select Food Category:", 
                   choices = colnames(df)[2:length(colnames(df))], 
@@ -32,7 +33,9 @@ ui <- fluidPage(
     ),
     
     mainPanel(
-      plotOutput("price_plot")
+      plotOutput("price_plot"),
+      br(),
+      DTOutput("price_table")
     )
   )
 )
@@ -46,14 +49,14 @@ server <- function(input, output) {
   
   output$price_plot <- renderPlot({
     ggplot(filtered_data(), aes(x = Date, y = .data[[input$food_category]])) +
-      geom_line(color = "blue", size = 1) +
-      annotate(geom = "label", x = as.Date("2020-01-01"), y = 310, 
+      geom_line(color = "darkgreen", size = 1) +
+      annotate(geom = "label", x = as.Date("2020-01-01"), y = 310,
                label = "COVID-19 (2020)", size = 4, family = "Roboto Condensed", hjust = 1, color = "red") +
-      annotate(geom = "label", x = as.Date("2021-01-20"), y = 325, 
+      annotate(geom = "label", x = as.Date("2021-01-20"), y = 325,
                label = "BIDEN ERA (2021)", size = 4, family = "Roboto Condensed", hjust = 1, color = "darkred") +
-      annotate(geom = "label", x = as.Date("2023-05-11"), y = 360, 
+      annotate(geom = "label", x = as.Date("2023-05-11"), y = 360,
                label = "End of COVID-19 (2023)", size = 4, family = "Roboto Condensed", hjust = 1, color = "darkgreen") +
-      annotate(geom = "label", x = as.Date("2025-05-20"), y = 370, 
+      annotate(geom = "label", x = as.Date("2025-05-20"), y = 370,
                label = "TRUMP ERA 2.0 (2025)", size = 4, family = "Roboto Condensed", hjust = 1, color = "darkred") +
       labs(title = paste("US Consumer Price Index Trend (Food at Home) of", input$food_category),
            subtitle = "	Seasonally Adjusted",
@@ -76,7 +79,7 @@ server <- function(input, output) {
               rownames = FALSE) %>%
       formatStyle(
         "Price",
-        background = styleColorBar(range(dat$Price, na.rm = TRUE), 'lightblue'),
+        background = styleColorBar(range(dat$Price, na.rm = TRUE), 'lightgreen'),
         backgroundSize = '98% 88%',
         backgroundRepeat = 'no-repeat',
         backgroundPosition = 'center'
@@ -87,3 +90,4 @@ server <- function(input, output) {
 
 # Run the application
 shinyApp(ui = ui, server = server)
+
